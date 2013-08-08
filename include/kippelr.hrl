dae40cf798cd2ad3270c1f89ebd0a79ee9fa3960
Sina Samavati <sina.samv@gmail.com>
2013-08-09 04:23:49 +0430

    Remove useless macros

--- include/kippelr.hrl
+++ include/kippelr.hrl
@@ -1,6 +1,4 @@
 -define(KIPPT, "https://kippt.com/api/").
--define(ACCOUNT, ?KIPPT ++ "account/").
--define(KIPPT_BASIC_AUTH, "https://~p:~p@kippt.com/api/").
 -define(TIMEOUT, 20000).
 
 -record(state, {url, headers}).
